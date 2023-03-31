e262cd9070b440c9e8693e7d00d9adfc369cd5dd
Muazzez Aydın <ayd.muazzez@gmail.com>
2023-04-01 02:15:37 +0300

    create: selection options screen and add to stack

--- app/screens/Selection/index.tsx
+++ app/screens/Selection/index.tsx
@@ -1,31 +1,26 @@
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {View} from 'react-native';
-import {Button} from '../../components';
+import {Button, Header} from '../../components';
 import style from './style';
 
 export const SelectionScreen: React.FC = () => {
   const navigation = useNavigation<any>();
-
-  const data = [
-    {title: 'Sırala', onPress: () => navigation.navigate('quiz-screen')},
-    {
-      title: 'Boşluk Doldur',
-      onPress: () => navigation.navigate('blank-screen'),
-    },
-    {title: 'Geri Dön', onPress: () => navigation.goBack()},
-  ];
-
   return (
-    <View style={style.root}>
-      {data.map((item, index) => (
+    <>
+      <Header title="Seçim Yapın" />
+      <View style={style.root}>
         <Button
-          key={index}
-          title={item.title}
-          onPress={item.onPress}
+          title={'Sırala'}
+          onPress={() => navigation.navigate('quiz-screen')}
           overStyle={style.button}
         />
-      ))}
-    </View>
+        <Button
+          title={'Boşluk Doldur'}
+          onPress={() => navigation.navigate('blank-screen')}
+          overStyle={style.button}
+        />
+      </View>
+    </>
   );
 };
