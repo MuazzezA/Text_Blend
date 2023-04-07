b15e5a49e6ae92ee41c5fdf4cf9163fe4949ddd5
Muazzez Aydın <ayd.muazzez@gmail.com>
2023-04-07 23:10:41 +0300

    feat : filteredParagraph context to home screen

--- app/screens/Home/index.tsx
+++ app/screens/Home/index.tsx
@@ -1,13 +1,23 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {ScrollView} from 'react-native';
 import {MainContext} from '../../context/MainContext';
 import {useNavigation} from '@react-navigation/native';
 import style from './style';
+import {sentenceRegex} from '../../utils/regex';
 import {Button, Card, TextInput} from '../../components';
 
 export const HomeScreen: React.FC = () => {
   const navigation = useNavigation<any>();
-  const {state, dispatch} = useContext(MainContext);
+  const {state, dispatch, dispatchFiltered} = useContext(MainContext);
+
+  const sentences: string[] = state.paragraph.split(sentenceRegex);
+  const filteredSentences: string[] = sentences.filter(sentence => {
+    return sentence.split(' ').length > 5;
+  });
+
+  const setFilteredSentences = async () => {
+    dispatchFiltered({type: 'set', payload: filteredSentences});
+  };
 
   const handleChange = (text: string) => {
     dispatch({type: 'set', payload: text});
@@ -21,7 +31,10 @@ export const HomeScreen: React.FC = () => {
       <TextInput value={state.paragraph} setValue={handleChange} />
       <Button
         title="OLUŞTUR"
-        onPress={() => navigation.navigate('selection-screen')}
+        onPress={() => {
+          setFilteredSentences();
+          navigation.navigate('selection-screen');
+        }}
         overStyle={style.button}
       />
     </ScrollView>
